83a428628111d3c0b793504a55b9fdc59cd348f8
sumitpagrawal <38112079+sumitpagrawal@users.noreply.github.com>
2018-04-09 15:07:59 +0530

    Update Default_Arguments.py

--- Default_Arguments.py
+++ Default_Arguments.py
@@ -2,4 +2,4 @@
 def print_something(name="someone",age="Unknown"):
     print("My Name is:", name, " and Age is: ", age)
 
-print_something(age=31, name="SUMIT")rfefgrfgrt
+print_something(age=31, name="SUMIT")
