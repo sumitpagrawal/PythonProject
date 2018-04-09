61479c383d455cd30bb6edbb96291fdf1b4ae2eb
sumitpagrawal <sumitpagrawal@gmail.com>
2018-04-09 17:27:56 +0530

    Commit with OUTPUT

---
+++ S3L10_BasicTypes-Strings.py
@@ -0,0 +1,27 @@
+print("Hello String")
+print('Hello')
+print("Don't do that")
+print('She said "I want this"')
+print('she said "Don\'t do that')
+print("Hello," + "Sumit")
+print("H" + "E" + "L" + "L" + "O")
+print("This costs " + str(6) + " Dollars")
+print("This costs " + str(6) + " Dollars")
+print("This costs " + str(6) + "  Dollars")
+print("This Costs " + str (6+5) + " Dollars")
+print("My name is " + "Hello:NICK:WORLD".split(":")[1])
+
+
+#OUTPUT
+#Hello String
+#Hello
+#Don't do that
+#She said "I want this"
+#she said "Don't do that
+#Hello,Sumit
+#HELLO
+#This costs 6 Dollars
+#This costs 6 Dollars
+#This costs 6  Dollars
+#This Costs 11 Dollars
+#My name is NICK
