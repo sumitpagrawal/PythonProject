738a350bd36adb606ed364edd36f958244166d99
sumitpagrawal <sumitpagrawal@gmail.com>
2018-04-11 18:44:00 +0530

    New Commit

---
+++ S3L23-If elif else Statements.py
@@ -0,0 +1,12 @@
+check = False
+
+if check == False:
+    print("It is False")
+
+elif check == "sumit":
+    print("hello")
+elif check == "Yo":
+    print("Yo")
+
+else:
+    print("It is actually True")
