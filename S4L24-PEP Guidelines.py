ba7aabed3e74e451e9986a6af5ced3f0d514f70d
sumitpagrawal <sumitpagrawal@gmail.com>
2018-04-15 07:44:29 +0530

    PEP Guidelines

---
+++ S4L24-PEP Guidelines.py
@@ -0,0 +1,25 @@
+#This section is about writing Python Syntax
+import os
+import sys
+
+def my_functions(one,two,
+                 three,four,
+                 five,six):
+    print("Hello")   #Line 10, 11 i.e. between 2 functions we need 2 blank lines
+    #i.e. between End of function and beginning of new function
+
+
+def second_function():
+    print("2nd Function")
+
+my_list = [1,2,
+           3,4,
+           5,6]
+
+print("Hello ", "Hi")
+
+x = 3*52 + 7*2
+check = True
+if check is True:
+    print("This is true")
+
